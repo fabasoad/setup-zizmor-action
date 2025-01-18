2775c04a79fffec7dc8430f7d08f6e6dbb80d02e
fabasoad <fabasoad@gmail.com>
2025-01-18 22:34:35 +0900

    Setup pip if needed

--- src/collect-info.sh
+++ src/collect-info.sh
@@ -9,43 +9,8 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 main() {
   input_force="${1}"
 
-  if command -v brew >/dev/null 2>&1 \
-    || command -v cargo >/dev/null 2>&1 \
-    || command -v pip >/dev/null 2>&1 \
-    || command -v pipx >/dev/null 2>&1 \
-    || command -v uv >/dev/null 2>&1 \
-    || command -v conda >/dev/null 2>&1 \
-    || command -v pacman >/dev/null 2>&1 \
-    || command -v zypper >/dev/null 2>&1; then
-    setup_cargo="false"
-  else
-    setup_cargo="true"
-  fi
-  echo "setup-cargo=${setup_cargo}" >> "$GITHUB_OUTPUT"
-
-  brew_installed=$(if command -v brew >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "brew-installed=${brew_installed}" >> "$GITHUB_OUTPUT"
-
-  cargo_installed=$(if command -v cargo >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "cargo-installed=${cargo_installed}" >> "$GITHUB_OUTPUT"
-
   pip_installed=$(if command -v pip >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "bash-installed=${pip_installed}" >> "$GITHUB_OUTPUT"
-
-  pipx_installed=$(if command -v pipx >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "pipx-installed=${pipx_installed}" >> "$GITHUB_OUTPUT"
-
-  uv_installed=$(if command -v uv >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "uv-installed=${uv_installed}" >> "$GITHUB_OUTPUT"
-
-  conda_installed=$(if command -v conda >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "conda-installed=${conda_installed}" >> "$GITHUB_OUTPUT"
-
-  pacman_installed=$(if command -v pacman >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "pacman-installed=${pacman_installed}" >> "$GITHUB_OUTPUT"
-
-  zypper_installed=$(if command -v zypper >/dev/null 2>&1; then echo true; else echo false; fi)
-  echo "zypper-installed=${zypper_installed}" >> "$GITHUB_OUTPUT"
+  echo "pip-installed=${pip_installed}" >> "$GITHUB_OUTPUT"
 
   bin_installed="false"
   if command -v zizmor >/dev/null 2>&1; then
--- src/install-zizmor.sh
+++ src/install-zizmor.sh
@@ -8,63 +8,10 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 
 main() {
   version="${1}"
-  if command -v brew >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      brew install zizmor
-    else
-      brew install zizmor@${version}
-    fi
-  elif command -v cargo >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      cargo install --locked zizmor
-    else
-      cargo install --locked zizmor --version ${version}
-    fi
-  elif command -v pip >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      pip install zizmor
-    else
-      pip install zizmor==${version}
-    fi
-  elif command -v pipx >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      pipx install zizmor
-    else
-      pipx install zizmor==${version}
-    fi
-  elif command -v uv >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      uv tool install zizmor
-    else
-      uv tool install zizmor==${version}
-    fi
-  elif command -v conda >/dev/null 2>&1; then
-    if [ "${version}" = "latest" ]; then
-      conda install conda-forge::zizmor
-    else
-      conda install conda-forge::zizmor=${version}
-    fi
-  elif command -v pacman >/dev/null 2>&1; then
-    pacman -Syu
-    if [ "${version}" = "latest" ]; then
-      pacman -S zizmor
-    else
-      pacman -S zizmor=${version}
-    fi
-  elif command -v zypper >/dev/null 2>&1; then
-    zypper refresh
-    zypper update
-    if [ "${version}" = "latest" ]; then
-      zypper install zizmor
-    else
-      zypper install zizmor=${version}
-    fi
+  if [ "${version}" = "latest" ]; then
+    pip install zizmor
   else
-    msg="No supported package managers found. Please install one of the"
-    msg="${msg} following tools before running this GitHub Action and try"
-    msg="${msg} again: brew, cargo, pip, pipx, uv, conda, pacman, zypper."
-    log_error "${msg}"
-    exit 1
+    pip install zizmor==${version}
   fi
 }
 
